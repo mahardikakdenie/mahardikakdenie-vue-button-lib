d4dcdf38e532c10d54e401d1f9ec6a843e83741a
MahardiikaKD <dikamahar884@gmail.com>
2023-09-15 10:02:43 +0700

    feat:update code btn and add new devDependencies

---
+++ src/components/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+<span>
+    <icon :icon="icon" />
+</span>
+</template>
+<script>
+import { Icon } from '@iconify/vue'
+export default {
+    name: 'IconComponent',
+    components: {
+        Icon
+    },
+    props: {
+        icon: {
+        type: String,
+        default: 'heroicons-outline:home'
+        }
+    }
+}
+</script>
+<style lang=""></style>
--- src/components/VueButton.vue
+++ src/components/VueButton.vue
@@ -1,22 +1,82 @@
 <template>
-<button 
-    :class="btnClass"
+<button
+    v-if="!isDiv && !isLink"
+    :class="[btnClass, {
+        'pointer-events-none': isLoading,
+        'btn-disabled': isDisabled,
+    }]"
+    class="btn-default"
+    :disabled="isDisabled"
+    v-bind="$attrs"
     @click="onClick"
 >
-    <slot> 
-        this is button 
-    </slot>
+    <template v-if="!isLoading && !$slor.default">
+        <span 
+            v-if="icon"
+            class="btn-text">
+            <span :class="[iconClass, {
+                'icon-position-right': iconPositionRight,
+                'icon-position-left': iconPositionRight && text
+            }]">
+                <icon :icon="icon" />
+            </span>
+        </span>
+    </template>
 </button>
 </template>
 
 <script>
+import Icon from '@/components/Icon.vue';
 export default {
     name: 'VueButton',
+    components: {
+        Icon
+    },
     props: {
         btnClass: {
             type: String,
             default: 'vue-button'
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+        isDiv: {
+            type: Boolean,
+            default: false,
+        },
+        isLink: {
+            type: Boolean,
+            default: false,
+        },
+        iconPosition: {
+            type: String,
+            default: ''
+        },
+        text: {
+            type: String,
+            default: ''
+        },
+        iconClass: {
+            type: String,
+            default: ''
+        },
+        icon: {
+            type: String,
+            default: ''
         }
+    },
+    computed: {
+        iconPositionRight() {
+            return this.iconPosition === 'right';
+        },
+        iconPositionLeft() {
+            return this.iconPosition === 'left';
+        },
     },
     methods: {
         onClick() {
@@ -33,5 +93,20 @@ export default {
     outline: none;
     border: none;
     cursor: pointer;
+}
+.btn-default {
+    display: inline-flex;
+    justify-content: center;
+}
+.pointer-events-none {
+    pointer-events: none;
+}
+.btn-disabled {
+    opacity: .4;
+    cursor: not-allowed;
+}
+.btn-text {
+    display: flex;
+    align-items: center;
 }
 </style>
